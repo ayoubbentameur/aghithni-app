c17c25b1e0dac17c111ce5a4ca6fd402d0d4cb0e
ayoub <MobileDev@2k22>
2023-02-02 12:21:53 +0100

    Adding Multiple Languages

--- app/src/main/java/com/benayoub/aghithni/IntroApp.java
+++ app/src/main/java/com/benayoub/aghithni/IntroApp.java
@@ -2,19 +2,47 @@ package com.benayoub.aghithni;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
-
+import android.util.DisplayMetrics;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class IntroApp extends AppCompatActivity {
     Timer timer;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences2=getSharedPreferences("lang",MODE_PRIVATE);
+        String language=sharedPreferences2.getString("language","");
+
+        switch (language){
+            case "en":
+                setLocale("en");
+                break;
+
+            case "fr":
+                setLocale("fr");
+                break;
+
+            case "ar":
+                setLocale("ar");
+                break;
+
+            default:
+                Locale.getDefault().getLanguage();
+                    break;
+
+        }
+
         setContentView(R.layout.activity_intro);
 
 
@@ -25,8 +53,8 @@ public class IntroApp extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
         }
+
 
         timer=new Timer();
         timer.schedule(new TimerTask() {
@@ -41,4 +69,15 @@ public class IntroApp extends AppCompatActivity {
 
 
     }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+
+    }
+
 }
--- app/src/main/java/com/benayoub/aghithni/LanguageSelection.java
+++ app/src/main/java/com/benayoub/aghithni/LanguageSelection.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 
@@ -11,6 +12,7 @@ import android.os.Bundle;
 
 import android.util.DisplayMetrics;
 
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,13 +20,6 @@ import java.util.Locale;
 
 public class LanguageSelection extends AppCompatActivity {
     TextView arabic,english,frensh;
-    Context context;
-    Resources resources;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,42 +28,39 @@ public class LanguageSelection extends AppCompatActivity {
                 arabic=findViewById(R.id.arabic_id);
                 english=findViewById(R.id.english_id);
                 frensh=findViewById(R.id.frensh_id);
-// Call this on the main thread as it may require Activity.restart()
-    savestate.retriveData(LanguageSelection.this,"key_1");
 
-        arabic.setOnClickListener(view -> {
-            context = LocaleHelper.setLocale(LanguageSelection.this, "ar");
-            resources = context.getResources();
-        });
+            arabic.setOnClickListener(view -> {
+                SharedPreferences.Editor editor=getSharedPreferences("lang",MODE_PRIVATE).edit();
+                editor.putString("language","ar");
+                editor.apply();
+                setLocale("ar");
+            });
 
+            english.setOnClickListener(view -> {
+                SharedPreferences.Editor editor=getSharedPreferences("lang",MODE_PRIVATE).edit();
+                editor.putString("language","en");
+                editor.apply();
+                setLocale("en");
+            });
 
-        english.setOnClickListener(view -> {
-
-            Locale myLocale = new Locale("en");
-            Resources res = getResources();
-            DisplayMetrics dm = res.getDisplayMetrics();
-            Configuration conf = res.getConfiguration();
-            conf.locale = myLocale;
-            res.updateConfiguration(conf, dm);
-            Intent refresh = new Intent(LanguageSelection.this, DashboardActivity.class);
-            finish();
-            startActivity(refresh);
-            finish();
-            savestate.insertData(getApplicationContext(),"key_1","en");
-            Toast.makeText(LanguageSelection.this, "Data Inserted", Toast.LENGTH_SHORT).show();
-            refresh.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        });
-
-        frensh.setOnClickListener(view -> {
-            context = LocaleHelper.setLocale(LanguageSelection.this, "fr-rFR");
-            resources = context.getResources();
-
-        });
+            frensh.setOnClickListener(view -> {
+                SharedPreferences.Editor editor=getSharedPreferences("lang",MODE_PRIVATE).edit();
+                editor.putString("language","fr");
+                editor.apply();
+                setLocale("fr");
+            });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        savestate.getPrefs(this);
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, DashboardActivity.class);
+        finish();
+        startActivity(refresh);
     }
+
 }
